55ac6f081cd492fdb045ff7b8b877978be04e93a
AikoCute <aikocute@icloud.com>
2022-08-04 00:05:04 +0700

    Update AikoR.sh

--- AikoR.sh
+++ AikoR.sh
@@ -12,79 +12,26 @@ cur_dir=$(pwd)
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${red}Error：${plain} This script must be run as root user!\n" && exit 1
 
-# check os
-if [[ -f /etc/redhat-release ]]; then
-    release="centos"
-elif cat /etc/issue | grep -Eqi "debian"; then
-    release="debian"
-elif cat /etc/issue | grep -Eqi "ubuntu"; then
-    release="ubuntu"
-elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
-    release="centos"
-elif cat /proc/version | grep -Eqi "debian"; then
-    release="debian"
-elif cat /proc/version | grep -Eqi "ubuntu"; then
-    release="ubuntu"
-elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
-    release="centos"
-else
-    echo -e "${red}System version not detected, please contact script author!${plain}\n" && exit 1
-fi
-
-arch=$(arch)
-
-if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
-  arch="64"
-elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
-  arch="arm64-v8a"
-else
-  arch="64"
-  echo -e "${red}No schema detected, use default schema: ${arch}${plain}"
-fi
-
-echo "Architecture System: ${arch}"
-
-if [ "$(getconf WORD_BIT)" != '32' ] && [ "$(getconf LONG_BIT)" != '64' ] ; then
-    echo "This software does not support 32-bit (x86) system, please use 64-bit (x86_64) system, if found wrong, please contact the author"
-    exit 2
-fi
-
-os_version=""
-
-# os version
-if [[ -f /etc/os-release ]]; then
-    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
-fi
-if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
-    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
-fi
-
-if [[ x"${release}" == x"centos" ]]; then
-    if [[ ${os_version} -le 6 ]]; then
-        echo -e "${red}Please use CentOS 7 or later!${plain}\n" && exit 1
-    fi
-elif [[ x"${release}" == x"ubuntu" ]]; then
-    if [[ ${os_version} -lt 16 ]]; then
-        echo -e "${red}Please use Ubuntu 16 or higher!${plain}\n" && exit 1
-    fi
-elif [[ x"${release}" == x"debian" ]]; then
-    if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red}Please use Debian 8 or later!${plain}\n" && exit 1
-    fi
-fi
-
 # install English
 english(){
     bash <(curl -Ls https://raw.githubusercontent.com/AikoCute-Offical/AikoR-Install/en/install.sh)
 }
 
+# install Chinese
+chinese(){
+    bash <(curl -Ls https://raw.githubusercontent.com/AikoCute-Offical/AikoR-Install/zh/install.sh)
+}
 
+# install vietnamese
+vietnamese(){
+    bash <(curl -Ls https://raw.githubusercontent.com/AikoCute-Offical/AikoR-Install/vi/install.sh)
+}   
 
 show_menu() {
     echo -e "
   ${green}AikoR Các tập lệnh quản lý phụ trợ，${plain}${red}không hoạt động với docker${plain}
 --- https://github.com/AikoCute-Offical/AikoR ---
-  ${green}0.${plain} Thoát AikoR
+  ${green}0.${plain} Exit Install AikoR
 ————————————————
   ${green}1.${plain} English
   ${green}2.${plain} 中文
